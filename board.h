e5f755de51eb2628b0520e3f28ee4afd9341cf09
soniacui <46662635+soniacui@users.noreply.github.com>
2019-11-23 22:42:58 -0500

    added some fields and methods, updated ctor
    added highScore, windowPtr, consolePtr, isGameOver(), restart(), and updated ctor

--- board.h
+++ board.h
@@ -7,8 +7,11 @@ class Tetromino;
 class LevelData;
 
 class Board:public Subject, public Observer{
+	static int highScore;
 	vector<vector<char>> grid;
 	vector<Tetromino*> tetrominoes;
+	Window *windowPtr;
+	Console *consolePtr;
 	vector<vector<int>> toDelete;
 	string currPunish;
 	string path;
@@ -18,12 +21,14 @@ class Board:public Subject, public Observer{
 	int score;
 	LevelData tetroFactory;
     public:
-	Board(int difficulty, int playerID, string path = "");
+	Board(int difficulty, int playerID, Window *windowPtr, Console *consolePtr, string path = "");
 	void notify(Subject &notifier) override; //once notified, react as appropriate based on caller, would always call getInfo(), relay state change with notifyObservers()
 	Info getInfo() const override; //creates and returns a BoardInfo
 	bool checkDropped(TetrominoInfo tetroInfo) const;  //checks to see if tetromino move signals end of turn
 	void clearLine(); //contains all end of turn effects, resets tetroFactory and currPunish to clean state, additional call to notifyObservers() for each removal
-
+	
+	bool isGameOver();
+	void restart();
 	// NOTE the blind effect may be difficult to implement
 
 	void sufferPunishment(string effect); //augments tetroFactory/grid to accomodate effect
