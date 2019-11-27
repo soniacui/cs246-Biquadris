8991312f4241847ed3ed1ad3d11ed0ae1ebddde0
soniacui <46662635+soniacui@users.noreply.github.com>
2019-11-26 21:17:14 -0500

    added seed field, default -1

--- level.h
+++ level.h
@@ -6,8 +6,9 @@ class Tetromino;
 class LevelData{
 	string path;
 	int difficulty;
+	int seed;
     public:
-	LevelData(int difficulty, string path = "");
+	LevelData(int difficulty, string path = "", int seed = -1);
 	Tetromino *generateTetromino(); //return a tetronimo pointer based on its difficulty
 	~LevelData() = default; //default destructor
 };
