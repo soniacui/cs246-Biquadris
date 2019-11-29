3551362a73884dac575610a3c5cb41f3d579ab60
soniacui <46662635+soniacui@users.noreply.github.com>
2019-11-29 00:17:36 -0500

    added move(), rotate(), drop()

--- tetromino.cc
+++ tetromino.cc
@@ -32,3 +32,151 @@ Info *Tetromino::getInfo() const {
     unique_ptr<TetrominoInfo> tInfo{ new TetrominoInfo(previously, absCoords, type, value, isDeleted) };
     return tInfo;
 }
+
+//notifyObservers called upon every state change (includes move, rotate, drop, and instances of notify)
+void Tetromino::move(string direction) { // move left/right
+
+	// update previously
+	for (int i = 0; i < absCoords.size(); ++i) {
+		previously[i][0] = absCoords[i][0];
+		previously[i][1] = absCoords[i][1];
+	}
+
+	if (direction == "right") {
+
+		// find right-most pixel 
+		int right_max = 0;
+		for (int i = 0; i < absCoords.size(); ++i) {
+			if (absCoords[i][0] > right_max) {
+				right_max = absCoords[i][0];
+			}
+		}
+
+		// check if the block can move right
+		if (right_max < currGrid[0].size() - 1) {  // ** i'm P SURE it's supposed to be -1
+			// if "right" is a valid move, shift all x coords 1 right
+			for (int i = 0; i < absCoords.size(); ++i) {
+				absCoords[i][0] = absCoords[i][0] + 1;
+			}
+
+		}
+
+
+	} else if (direction == "left") { 
+
+		// find left-most pixel 
+		int left_max = currGrid[0].size() - 1;
+		for (int i = 0; i < absCoords.size(); ++i) {
+			if (absCoords[i][0] < left_max) {
+				left_max = absCoords[i][0];
+			}
+		}
+
+		// check if the block can move left
+		if (left_max > 0) { 
+			// if "left" is a valid move, shift all x coords 1 left
+			for (int i = 0; i < absCoords.size(); ++i) {
+				absCoords[i][0] = absCoords[i][0] - 1;
+			}
+
+		}
+
+
+	} else if (direction == "down") {  // dropping to bottom most row or into another block does not end turn
+
+		// find bottom-most pixel 
+		int bottom_max = 0;
+		for (int i = 0; i < absCoords.size(); ++i) {
+			if (absCoords[i][1] > bottom_max) {
+				bottom_max = absCoords[i][1];
+			}
+		}
+
+		// check if the block can move down
+		if (bottom_max > 0) { 
+			// if "down" is a valid move, shift all y coords 1 down
+			for (int i = 0; i < absCoords.size(); ++i) {
+				absCoords[i][1] = absCoords[i][0] - 1;
+			}	
+
+		}
+
+
+	}
+
+	notify();  // notify observers
+
+}
+
+void Tetromino::rotate(string direction) {
+
+	// update previously
+	for (int i = 0; i < absCoords.size(); ++i) {
+		previously[i][0] = absCoords[i][0];
+		previously[i][1] = absCoords[i][1];
+	}
+
+	if (direction == "clockwise") {
+		//update abscoords
+		
+		//find bottom-left corner
+		//check if valid move
+		//some rotating algorithm
+
+	} else if (direction == "counterclockwise") {
+		//update previously
+		//update abscoords
+
+	}
+
+	notify();
+}
+
+
+void Tetromino::drop() {   // A TURN IS NOT OVER UNTIL THE BLOCK HAS BEEN DROPPED OR THE BLOCK HAS BEEN 'HEAVYED' TO THE BOTTOM-MOST POSSIBLE
+
+	// update previously
+	for (int i = 0; i < absCoords.size(); ++i) {
+		previously[i][0] = absCoords[i][0];
+		previously[i][1] = absCoords[i][1];
+	}
+
+	//update abscoords (check against currgrid's highest tile)
+	//shift all tetro's coords 1 down until at lowest possible
+
+	//find current block's left most pixel
+	int left_max = currGrid[0].size() - 1;
+	for (int i = 0; i < absCoords.size(); ++i) {
+		if (absCoords[i][0] < left_max) {
+			left_max = absCoords[i][0];
+		}
+	}
+
+	//find current block's right most pixel
+	int right_max = 0;
+	for (int i = 0; i < absCoords.size(); ++i) {
+		if (absCoords[i][0] > right_max) {
+			right_max = absCoords[i][0];
+		}
+	}
+
+	// find bottom-most pixel that is under current block 
+	int bottom_max = 0;
+	for (int i = 0; i < currGrid.size(); ++i) {  // rows of currGrid
+		for (int j = left_max; j <= right_max; ++j) {  // cols of current block
+			if (currGrid[i][j] != ' ') {
+				bottom_max = i;
+			}
+		
+		}
+	}
+
+	// shift block as down as possible
+	for (int i = 0; i < absCoords.size(); ++i) {
+		absCoords[i][1] = currGrid.size() - absCoords[i][0] + bottom_max;
+	}	
+
+	notify();
+}
+
+
