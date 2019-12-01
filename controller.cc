4667850aaebdc0d74465b9c11a5f6981a3364d04
soniacui <46662635+soniacui@users.noreply.github.com>
2019-12-01 18:09:15 -0500

    completed controller.cc

--- controller.cc
+++ controller.cc
@@ -7,7 +7,8 @@
 #include "board.h"
 #include "tetromino.h"
 #include "info.h"
-// do i need to include something so i can print the board
+#include "textdisplay.h"
+//#include "graphicsdisplay.h"
 
 using namespace std;
 
@@ -73,11 +74,7 @@ void Controller::readArgs() {
 
 }
 
-// read command from stdin and call the corresponding action   
-void Controller::readStdin() {
-
-  // read input until EOF
-  while (cin >> s) {
+void Controller::matchCmd(string s) {
 
     cout << "creating stream: " << endl;
     istringstream iss(s);
@@ -92,9 +89,8 @@ void Controller::readStdin() {
 
     }
 
-
     try { //throw smth if failed
-    	int matches = 0;
+	int matches = 0;
    	int matched_index = 0;
 	
 	cout << "checking matches: " << endl;
@@ -116,226 +112,135 @@ void Controller::readStdin() {
 	}
 
 	cmd = commands.at(matched_index);
-
-	cout << "matching command: " << endl;
+    if (!b1.menu && !b2.menu) {
+    	cout << "matching command: " << endl;
     	if (cmd == commands.at(0)) { 
        		cout << "command: left" << endl;
-		for (int i = 0; i < multiplier; ++i) {
-			//check who's turn it is
-			//perform action 
-			if (b1.isTurn) {
-				b1.performAction("left");
-				if (!b1.isTurn) {      //if turn is over, stop performing action even if mutliplier isn't over
-					break;
-				}
-			} else {
-				b2.performAction("left");
-				if (!b2.isTurn) {      //if turn is over, stop performing action even if mutliplier isn't over
-					break;
-				}
-			}
-		}
+		multAction(multiplier, "left");
    	}
    	else if (cmd == commands.at(1)) {
     		cout << "command: right" << endl;
-		for (int i = 0; i < multiplier; ++i) {
-			//check who's turn it is
-			//perform action 
-			if (b1.isTurn) {
-				b1.performAction("right");
-				if (b1.isTurn) {      //if turn is over, stop performing action even if mutliplier isn't over
-					break;
-				}
-			} else {
-				b2.performAction("right");
-				if (b2.isTurn) {      //if turn is over, stop performing action even if mutliplier isn't over
-					break;
-				}
-			}
-		}
-   	}
+		multAction(multiplier, "right");   	
+	}
    	else if (cmd == commands.at(2)) {
     	        cout << "command: down" << endl;
-		for (int i = 0; i < multiplier; ++i) {
-			//check who's turn it is
-			//perform action 
-			if (b1.isTurn) {
-				b1.performAction("down");
-				if (b1.isTurn) {      //if turn is over, stop performing action even if mutliplier isn't over
-					break;
-				}
-			} else {
-				b2.performAction("down");
-				if (b2.isTurn) {      //if turn is over, stop performing action even if mutliplier isn't over
-					break;
-				}
-			}
-		}
+		multAction(multiplier, "down");  
     	}
     	else if (cmd == commands.at(3)) {
     		cout << "command: clockwise" << endl;
-		for (int i = 0; i < multiplier; ++i) {
-			//check who's turn it is
-			//perform action 
-			if (b1.isTurn) {
-				b1.performAction("clockwise");
-				if (b1.isTurn) {      //if turn is over, stop performing action even if mutliplier isn't over
-					break;
-				}
-			} else {
-				b2.performAction("clockwise");
-				if (b2.isTurn) {      //if turn is over, stop performing action even if mutliplier isn't over
-					break;
-				}
-			}
-		}
+		multAction(multiplier, "clockwise"); 
  	}
    	else if (cmd == commands.at(4)) {
     	 	cout << "command: counterclockwise" << endl;
-		for (int i = 0; i < multiplier; ++i) {
-			//check who's turn it is
-			//perform action 
-			if (b1.isTurn) {
-				b1.performAction("counterclockwise");
-				if (b1.isTurn) {      //if turn is over, stop performing action even if mutliplier isn't over
-					break;
-				}
-			} else {
-				b2.performAction("counterclockwise");
-				if (b2.isTurn) {      //if turn is over, stop performing action even if mutliplier isn't over
-					break;
-				}
-			}
-		}
+		multAction(multiplier, "counterclockwise"); 
     	}
     	else if (cmd == commands.at(5)) {
     	        cout << "command: drop" << endl;
-			if (b1.isTurn) {
-				b1.performAction("drop");
-				if (b1.isTurn) {      //if turn is over, stop performing action even if mutliplier isn't over
-					break;
-				}
-			} else {
-				b2.performAction("drop");
-				if (b2.isTurn) {      //if turn is over, stop performing action even if mutliplier isn't over
-					break;
-				}
-			}
+		multAction(1, "drop"); //ignores multiplier
   	}
  	else if (cmd == commands.at(6)) {
     	        cout << "command: levelup" << endl;
-		for (int i = 0; i < multiplier; ++i) {
-			//check who's turn it is
-			//perform action 
-			if (b1.isTurn) {
-				b1.performAction("levelup");
-				if (b1.isTurn) {      //if turn is over, stop performing action even if mutliplier isn't over
-					break;
-				}
-			} else {
-				b2.performAction("levelup");
-				if (b2.isTurn) {      //if turn is over, stop performing action even if mutliplier isn't over
-					break;
-				}
-			}
-		}
+		multAction(multiplier, "levelup"); 
   	}
   	else if (cmd == commands.at(7)) {
     	        cout << "command: leveldown" << endl;
-		for (int i = 0; i < multiplier; ++i) {
-			//check who's turn it is
-			//perform action 
-			if (b1.isTurn) {
-				b1.performAction("leveldown");
-				if (b1.isTurn) {      //if turn is over, stop performing action even if mutliplier isn't over
-					break;
-				}
-			} else {
-				b2.performAction("leveldown");
-				if (b2.isTurn) {      //if turn is over, stop performing action even if mutliplier isn't over
-					break;
-				}
-			}
-		}
+		multAction(multiplier, "leveldown"); 
   	}
   	else if (cmd == commands.at(8)) {
 		string file;
     	 	cout << "command: norandom" << endl;
 		cin >> file;       
-		if (b1.isTurn) {
-			b1.toggleRandom(file);
-		} else {
-			b2.toggleRandom(file);
-		}
-
+		multAction(1, "norandom", file);
    	}
   	else if (cmd == commands.at(9)) {
     	 	cout << "command: random" << endl;
-    	 	if (b1.isTurn) {
-			b1.toggleRandom("");
-		} else {
-			b2.toggleRandom("");
-		}	
+		multAction(1, "random");	
   	}
    	else if (cmd == commands.at(10)) {
     	 	cout << "command: sequence" << endl;
 		string file;
 		cin >> file;
-		ifstream ifs {file};
-		//call controller with the commands in this file
+		multAction(1, "sequence", file);
   	}
-   	else if (cmd == commands.at(11)) {                             //JK "FORCEBLOCK" IS FOR FORCING THE OTHER PERSON'S BLOCK
-    	 	cout << "command: I" << endl;                          //IDK HOW TO FORCE MY OWN BLOCK YET
-		for (int i = 0; i < multiplier; ++i) {			// ok jk this is fine; i just have to worry about taking input for SufferPunishment
-			if (b1.isTurn) {
-				b1.performAction("forceI");
-				if (!b1.isTurn) {
-					break;
-				}
-			} else {
-				b2.performAction("forceI");
-				if (!b2.isTurn) {
-					break;
-				}
-			}
-		}
+   	else if (cmd == commands.at(11)) {                             
+    	 	cout << "command: I" << endl;                          
+		multAction(multiplier, "forceI");
   	}
   	else if (cmd == commands.at(12)) {
     	 	cout << "command: J" << endl;
+		multAction(multiplier, "forceJ");
    	}
     	else if (cmd == commands.at(13)) {
     	 	cout << "command: L" << endl;
+		multAction(multiplier, "forceL");
    	}
    	else if (cmd == commands.at(14)) {
     	 	cout << "command: O" << endl;
+		multAction(multiplier, "forceO");
    	}
    	else if (cmd == commands.at(15)) {
     	 	cout << "command: S" << endl;
+		multAction(multiplier, "forceS");
    	}
     	else if (cmd == commands.at(16)) {
     	 	cout << "command: Z" << endl;
+		multAction(multiplier, "forceZ");
    	}
    	else if (cmd == commands.at(17)) {
     	 	cout << "command: T" << endl;
+		multAction(multiplier, "forceT");
    	}
    	else if (cmd == commands.at(18)) {
     	 	cout << "command: restart" << endl;
-		b1.restart();
-		b2.restart();
+		multAction(1, "restart");
    	} else {
 		throw 1;
 	}
+     } else {
+	   if (cmd == commands.at(19)) {
+		cout << "command: blind" << endl;
+		multAction(1, "blind");
+	   } else if (cmd == commands.at(20)) {
+		cout << "command: heavy" << endl;
+		multAction(1, "heavy");
+	   } else if (cmd == commands.at(18)) {
+		cout << "command: force" << endl;
+		string type;
+		cin >> type;
+		multAction(1, "force" + type);
+	   }
+     }
         cout << display;
         if (checkGameOver()) {  // if game over, instant restart?
-        	b1.restart();
-		b2.restart();
-		b1.attach(b2);
-		b2.attach(b1);
+		multAction(1, restart);
    	}
+
     }
+   
     catch (int e) {
         cout << "Unrecognized command. Please try again." << endl;
     }
+
+}
+
+// read command from stdin and call the corresponding action   
+void Controller::readStdin() {
+
+  // read input until EOF
+  while (cin >> s) {
+	matchCmd(s);
+  }
+}
+
+// create file and read from file, call corresponding action
+void Controller::readFile(string file) {
+
+  // read input from file until EOF
+  ifstream ifs {file};
+  string s;
+  while (ifs >> s) {
+     matchCmd(s);
+   
   }
 }
 
@@ -343,17 +248,39 @@ bool Controller::checkGameOver() {
 	return (b1.isGameOver() || b2.isGameOver());
 }
 
-void Controller::multAction(int multiplier, string action) {
-	for (int i = 0; i < multiplier; ++i) {
+void Controller::multAction(int multiplier, string action, string file = "") {
+	if (action == "norandom" || action == "random") {
+		if (b1.isTurn) {
+			b1.toggleRandom(file);
+		} else {
+			b2.toggleRandom(file);
+		}
+	} else if (action == "sequence") {
+		readFile(file);
+	} else if (action == "restart") { //restart
+        	b1.restart();
+		b2.restart();
+		b1.attach(b2);
+		b2.attach(b1);
+	} else if (action == "blind" || action == "heavy") { //will not end turn
 		if (b1.isTurn) {
 			b1.performAction(action);
-			if (!b1.isTurn) {
-				break;
-			}
 		} else {
 			b2.performAction(action);
-			if (!b2.isTurn) {
-				break;
+		}
+	} else {   //can potentially end turns
+		//move/rotate/drop/force
+		for (int i = 0; i < multiplier; ++i) {
+			if (b1.isTurn) {
+				b1.performAction(action);
+				if (!b1.isTurn) {
+					break;
+				}
+			} else {
+				b2.performAction(action);
+				if (!b2.isTurn) {
+					break;
+				}
 			}
 		}
 	}
