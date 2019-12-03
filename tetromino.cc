49c3bdcef9ee7b7209f7c2fd4cf61fc49c20bb8c
soniacui <46662635+soniacui@users.noreply.github.com>
2019-12-03 00:23:56 -0500

    aa

--- tetromino.cc
+++ tetromino.cc
@@ -28,42 +28,42 @@ Tetromino::Tetromino(vector<vector<char>> currGrid, int difficulty, char type) :
 }
 
 IBlock::IBlock(vector<vector<char>> grid, int difficulty, char type) : Tetromino(grid, difficulty, type) {
-    absCoords = { {0, 14}, {1, 14}, {2, 14}, {3, 14} };
+	absCoords = vector<vector<int>> { vector<int>{0, 14}, vector<int>{0, 13}, vector<int>{0, 12}, vector<int>{0, 11} };
 }
 IBlock::~IBlock() {}
 
 JBlock::JBlock(vector<vector<char>> grid, int difficulty, char type) : Tetromino(grid, difficulty, type) {
-    absCoords = { {0, 15}, {0, 14}, {1, 14}, {2, 14} };
+    absCoords = vector<vector<int>>{ vector<int>{0, 14}, vector<int>{0, 13}, vector<int>{0, 12}, vector<int>{1, 14} };
 }
 JBlock::~JBlock() {}
 
 LBlock::LBlock(vector<vector<char>> grid, int difficulty, char type) : Tetromino(grid, difficulty, type) {
-    absCoords = { {0, 14}, {1, 14}, {2, 14}, {2, 15} };
+    absCoords = vector<vector<int>>{ vector<int>{0, 13}, vector<int>{1, 13}, vector<int>{2, 13}, vector<int>{2, 14} };
 }
 LBlock::~LBlock() {}
 
 OBlock::OBlock(vector<vector<char>> grid, int difficulty, char type) : Tetromino(grid, difficulty, type) {
-    absCoords = { {0, 14}, {1, 14}, {0, 15}, {1, 15} };
+    absCoords = vector<vector<int>>{ vector<int>{0, 14}, vector<int>{0, 13}, vector<int>{1, 14}, vector<int>{1, 13} };
 }
 OBlock::~OBlock() {}
 
 SBlock::SBlock(vector<vector<char>> grid, int difficulty, char type) : Tetromino(grid, difficulty, type) {
-    absCoords = { {0, 14}, {1, 14}, {1, 15}, {2, 15} };
+    absCoords = vector<vector<int>>{ vector<int>{0, 13}, vector<int>{1, 13}, vector<int>{1, 14}, vector<int>{2, 14} };
 }
 SBlock::~SBlock() {}
 
 ZBlock::ZBlock(vector<vector<char>> grid, int difficulty, char type) : Tetromino(grid, difficulty, type) {
-    absCoords = { {0, 15}, {1, 15}, {1, 14}, {2, 14} };
+    absCoords = vector<vector<int>>{ vector<int>{0, 14}, vector<int>{2, 13}, vector<int>{1, 14}, vector<int>{1, 13} };
 }
 ZBlock::~ZBlock() {}
 
 TBlock::TBlock(vector<vector<char>> grid, int difficulty, char type) : Tetromino(grid, difficulty, type) {
-    absCoords = { {0, 15}, {1, 15}, {2, 15}, {1, 14} };
+    absCoords = { {0, 14}, {1, 14}, {2, 14}, {1, 13} };
 }
 TBlock::~TBlock() {}
 
 StarBlock::StarBlock(vector<vector<char>> grid, int difficulty, char type) : Tetromino(grid, difficulty, type) {
-    absCoords = { {5, 14} };
+    absCoords = vector<vector<int>>{ vector<int>{5, 14} };
 }
 StarBlock::~StarBlock() {}
 
@@ -112,26 +112,58 @@ void Tetromino::updatePreviously() {
 	}
 }
 
-
-// moves a Tetromino piece 1 down 
-void Tetromino::moveDown() {
-	// find bottom-most pixel 
-	int bottom_max = 0;
+int Tetromino::findLeftMost() {
+	// find left-most pixel 
+	int left_max = currGrid[0].size() - 1;
 	for (int i = 0; i < absCoords.size(); ++i) {
-		if (absCoords[i][1] > bottom_max) {
+		if (absCoords[i][0] < left_max) {
+			left_max = absCoords[i][0];
+		}
+	}
+	return left_max;
+}
+
+int Tetromino::findRightMost() {
+	// find right-most pixel 
+	int right_max = 0;
+	for (int i = 0; i < absCoords.size(); ++i) {
+			if (absCoords[i][0] > right_max) {
+			right_max = absCoords[i][0];
+		}
+	}
+	return right_max;
+}
+
+int Tetromino::findBottomMost() {
+	// find bottom-most pixel 
+	int bottom_max = currGrid.size() - 1;
+	for (int i = 0; i < absCoords.size(); ++i) {
+		if (absCoords[i][1] < bottom_max) {
 			bottom_max = absCoords[i][1];
 		}
 	}
+	return bottom_max;
+}
+
+// moves a Tetromino piece 1 down 
+void Tetromino::moveDown() {
+	int bottom_most = findBottomMost();
 
 	// check if the block can move down
-	if (bottom_max > 1) {
-	      cout << "CAN STILL MOVE DOWN" << endl;	
-		// if "down" is a valid move, shift all y coords 1 down
+	if (bottom_most > 1) {
+	        cout << "CAN STILL MOVE DOWN" << endl;	
+		//check if block will collide with anything
 		for (int i = 0; i < absCoords.size(); ++i) {
-			absCoords[i][1] = absCoords[i][1] - 1;
-		}	
-
+			if (currGrid[absCoords[i][0]][absCoords[i][1] - 1] != ' ') {
+				return;
+			}
+		}
 	}
+	// if "down" is a valid move, shift all y coords 1 down
+	for (int i = 0; i < absCoords.size(); ++i) {
+		absCoords[i][1] = absCoords[i][1] - 1;
+	}	
+
 
 }
 
@@ -141,43 +173,42 @@ void Tetromino::move(string direction) { // move left/right
 	updatePreviously();
 	cout << "cccccccc" << previously.size() << endl;
 	if (direction == "right") {
-
-		// find right-most pixel 
-		int right_max = 0;
-		for (int i = 0; i < absCoords.size(); ++i) {
-			if (absCoords[i][0] > right_max) {
-				right_max = absCoords[i][0];
+		int right_most = findRightMost();
+	
+		// check if the block can move right
+		if (right_most < currGrid[0].size() - 1) {
+			//check if block will collide with anything
+			for (int i = 0; i < absCoords.size(); ++i) {
+				if (currGrid[absCoords[i][0] + 1][absCoords[i][1]] != ' ') {
+					return;
+				}
 			}
 		}
-
-		// check if the block can move right
-		if (right_max < currGrid[0].size() - 1) { 
-			// if "right" is a valid move, shift all x coords 1 right
-			for (int i = 0; i < absCoords.size(); ++i) {
-				absCoords[i][0] = absCoords[i][0] + 1;
-			}
-
+ 
+		// if "right" is a valid move, shift all x coords 1 right
+		for (int i = 0; i < absCoords.size(); ++i) {
+			absCoords[i][0] = absCoords[i][0] + 1;
 		}
 
 
 	} else if (direction == "left") { 
 
-		// find left-most pixel 
-		int left_max = currGrid[0].size() - 1;
-		for (int i = 0; i < absCoords.size(); ++i) {
-			if (absCoords[i][0] < left_max) {
-				left_max = absCoords[i][0];
-			}
-		}
+		int left_most = findLeftMost();
 
 		// check if the block can move left
-		if (left_max > 0) { 
-			// if "left" is a valid move, shift all x coords 1 left
+		if (left_most > 0) { 
+			//check if block will collide with anything
 			for (int i = 0; i < absCoords.size(); ++i) {
-				absCoords[i][0] = absCoords[i][0] - 1;
+				if (currGrid[absCoords[i][0] - 1][absCoords[i][1]] != ' ') {
+					return;
+				}
 			}
-
 		}
+		// if "left" is a valid move, shift all x coords 1 left
+		for (int i = 0; i < absCoords.size(); ++i) {
+			absCoords[i][0] = absCoords[i][0] - 1;
+		}
+
 
 
 	} else if (direction == "down") {  // down-ing to bottom most row or into another block does not end turn	
@@ -201,61 +232,80 @@ void Tetromino::rotate(string direction) {
 
 	if (direction == "clockwise") {
 
-		//check if valid move ******************
+		vector<vector<int>> check = absCoords; // a "trial" vector to see if the rotate is valid
 
-		// find left-most pixel 
-		int left_max = currGrid[0].size() - 1;
-		for (int i = 0; i < absCoords.size(); ++i) {
-			if (absCoords[i][0] < left_max) {
-				left_max = absCoords[i][0];
+		// shift trial vector to origin, and rotate clockwise
+		for (int i = 0; i < check.size(); ++i) {
+			check.emplace_back({absCoords[i][1] - bottom_most, -(absCoords[i][0] - left_most)}); 
+		}
+
+		int left_most = findLeftMost();        //find left most before shifting
+	        int bottom_most = findBottomMost();     //find bottom most before shifting
+
+		// find right-most pixel after rotating
+		int new_left_most = currGrid[0].size() - 1;
+		for (int i = 0; i < check.size(); ++i) {
+			if (check[i][0] > new_left_most) {
+				new_left_most = check[i][0];
 			}
 		}
-
-		// find bottom-most pixel 
-		int bottom_max = currGrid.size() - 1;
-		for (int i = 0; i < absCoords.size(); ++i) {
-			if (absCoords[i][1] < bottom_max) {
-				bottom_max = absCoords[i][1];
+		// find bottom-most pixel after rotating
+		int new_bottom_most = currGrid.size() - 1;
+		for (int i = 0; i < check.size(); ++i) {
+			if (check[i][1] < new_bottom_most ) {
+				new_bottom_most = check[i][1];
 			}
 		}
-		//bottom-left-most = (left-most, bottom-most)
-		
-
-		vector<vector<int>> temp;
-		for (int i = 0; i < absCoords.size(); ++i) {
-			absCoords[i][0] = absCoords[i][0] - left_max; //shift x to origin
-			absCoords[i][1] = absCoords[i][1] - bottom_max; //shift y to origin
-            vector <int> temp2 = { absCoords[i][1], -(absCoords[i][0]) };
-			temp.emplace_back(temp2); // (y, -x)
-		}
-		absCoords = temp; // rotate	
-
-		// find NEW left-most pixel 
-		int new_left_max = currGrid[0].size() - 1;
-		for (int i = 0; i < absCoords.size(); ++i) {
-			if (absCoords[i][0] < left_max) {
-				left_max = absCoords[i][0];
+	
+		//shift back to desired location and check if valid move
+		for (int i = 0; i < check.size(); ++i) {
+			check[i][0] = check[i][0] + abs(left_most - new_left_most); 
+			check[i][1] = check[i][1] + abs(bottom_most - new_bottom_most);
+			if ((check[i][0] < 0) && (check[i][0] > currGrid[0].size() - 1) &&     //x is out of bounds
+			    (check[i][1] > currGrid.size() - 1) && (check[i][1] < 0) &&       //y is out of bounds
+			    (currGrid[check[i][0]][check[i][1]] != ' ')) {                  //rotated block overlaps
+				return;     //if move is not valid, exit
 			}
 		}
-
-		// find NEW bottom-most pixel 
-		int new_bottom_max = currGrid.size() - 1;
-		for (int i = 0; i < absCoords.size(); ++i) {
-			if (absCoords[i][1] < bottom_max) {
-				bottom_max = absCoords[i][1];
-			}
-		}
-
-		//shift back
-		for (int i = 0; i < absCoords.size(); ++i) {
-			absCoords[i][0] = absCoords[i][0] + (left_max - new_bottom_max);  // abs????*********
-			absCoords[i][1] = absCoords[i][1] + (bottom_max - new_bottom_max);
-		}
+		absCoords = check; //if move was valid, copy the 'check' vector
 
 	} else if (direction == "counterclockwise") {
-		//update previously
-		//update abscoords
+		vector<vector<int>> check = absCoords; // a "trial" vector to see if the rotate is valid
 
+		// shift trial vector to origin, and rotate clockwise
+		for (int i = 0; i < check.size(); ++i) {
+			check.emplace_back({-(absCoords[i][1] - bottom_most), absCoords[i][0] - left_most}); 
+		}
+
+		int left_most = findLeftMost();        //find left most before shifting
+	        int bottom_most = findBottomMost();     //find bottom most before shifting
+
+		// find right-most pixel after rotating
+		int new_left_most = currGrid[0].size() - 1;
+		for (int i = 0; i < check.size(); ++i) {
+			if (check[i][0] > new_left_most) {
+				new_left_most = check[i][0];
+			}
+		}
+		// find bottom-most pixel after rotating
+		int new_bottom_most = currGrid.size() - 1;
+		for (int i = 0; i < check.size(); ++i) {
+			if (check[i][1] < new_bottom_most ) {
+				new_bottom_most = check[i][1];
+			}
+		}
+	
+		//shift back to desired location and check if valid move
+		for (int i = 0; i < check.size(); ++i) {
+			check[i][0] = check[i][0] + abs(left_most - new_left_most); 
+			check[i][1] = check[i][1] + abs(bottom_most - new_bottom_most);
+			if ((check[i][0] < 0) && (check[i][0] > currGrid[0].size() - 1) &&     //x is out of bounds
+			    (check[i][1] > currGrid.size() - 1) && (check[i][1] < 0) &&       //y is out of bounds
+			    (currGrid[check[i][0]][check[i][1]] != ' ')) {                  //rotated block overlaps
+				return;     //if move is not valid, exit
+			}
+		}
+		absCoords = check; //if move was valid, copy the 'check' vector
 	}
 
 	for (int i = 0; i < speed; ++i) {
@@ -272,32 +322,25 @@ void Tetromino::drop() {
 
 	//update abscoords (check against currgrid's highest tile)
 	//shift all tetro's coords 1 down until at lowest possible
-
-	//find current block's left most pixel
-	int left_max = currGrid[0].size() - 1;
-	for (int i = 0; i < absCoords.size(); ++i) {
-		if (absCoords[i][0] < left_max) {
-			left_max = absCoords[i][0];
-		}
-	}
-
-	//find current block's right most pixel
-	int right_max = 0;
-	for (int i = 0; i < absCoords.size(); ++i) {
-		if (absCoords[i][0] > right_max) {
-			right_max = absCoords[i][0];
-		}
-	}
+	
+	int left_most = findLeftMost();
+	int right_most = findRightMost();
 
 	// find bottom-most pixel that is under current block 
-	int smallest_diff = currGrid.size() - 1;
+	int smallest_diff = currGrid.size();
 	int diff = 0;
-	for (int i = left_max; i <= right_max; ++i) { // cols of current block
+        bool start_count;
+	for (int i = left_most; i <= right_most; ++i) { // cols of current block
 		diff = 0;
-		for (int j = 0; j < currGrid.size(); ++j) { // rows of currGrid
-			if (currGrid[i][j] == ' ') {
+		start_count = false;
+		for (int j = currGrid.size() - 2; j >= 0; --j) { // rows of currGrid
+			if ((currGrid[i][j] == ' ') && (currGrid[i][j + 1] != ' ')) {
+				start_count = true;
+			}
+			if (start_count && (currGrid[i][j] == ' ')) {
 				++diff;
-			} else {
+			}
+			if (start_count && (currGrid[i][j] != ' ')) {
 				break;
 			}
 		}
@@ -308,10 +351,11 @@ void Tetromino::drop() {
 
 	// shift block as down as possible
 	for (int i = 0; i < absCoords.size(); ++i) {
-		absCoords[i][1] = absCoords[i][1] - smallest_diff + 1;
-	}	
+		absCoords[i][1] = absCoords[i][1] - smallest_diff;
+	}
+	
 	isDropped = true;
-	notifyObservers();
+	notifyObservers(); 
 }
 
 Tetromino::~Tetromino() {}
