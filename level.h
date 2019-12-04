b4cb56d770ecb04e556e25ed00e0e2df6b795aac
soniacui <46662635+soniacui@users.noreply.github.com>
2019-12-03 20:41:36 -0500

    added playerID field

--- level.h
+++ level.h
@@ -7,11 +7,12 @@ class Tetromino;
 class LevelData{
 	std::string path; // file name
 	int difficulty;
+	int playerID;
 	int seed;
 	int index; // index of the seq vector we read from
 	std::vector<std::string> seq; // holds the commands from the input file
     public:
-	/*explicit*/ LevelData(int difficulty, std::string path = "", int seed = -1);
+	/*explicit*/ LevelData(int difficulty, int playerID, std::string path = "", int seed = -1);
 	Tetromino *generateTetromino(std::vector<std::vector<char>> grid); //return a tetronimo pointer based on its difficulty
     Tetromino *forceGenerate(std::vector<std::vector<char>> grid, std::string typeForced); //returns a tetromino pointer of the desired type{L, J, O ... *}
 	~LevelData() = default; //default destructor
